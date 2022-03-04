21ae234eaec34d535046ab66b96106566fb47113
polarbub <71790726+polarbub@users.noreply.github.com>
2022-03-03 20:57:44 -0600

    CMake updates and trying to fix PSL

--- src/include/psl/psl.h
+++
@@ -1,5 +0,0 @@
-#include <chrono>
-#include <thread>
-//TODO: Make git submodules
-#define psl_sleep(ms) std::this_thread::sleep_for(std::chrono::milliseconds(ms))
-#define pls_getArrayLength(array) sizeof(array)/sizeof(array[0])
---
+++ src/libraries/PSL/src/psl.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "psl.h"
+
